dda611c038092877a5b241d84c5180bdc9d49d3c
SoundStrategies <SoundStrategies@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-21 02:29:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/soundst-linkmgr/trunk@453668 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ links-manager-rewrite-rules.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Description: Allow Links Manager run inside WP.
+Author: Sound Strategies
+Version: 1.1
+Author URI: http://www.soundst.com/
+*/
+
+add_filter('rewrite_rules_array', 'wp_insertLMRewriteRules');
+add_filter('query_vars', 'wp_insertLMRewriteQueryVars');
+add_filter('init', 'flushLMRules');
+
+// Remember to flush_rules() when adding rules
+function flushLMRules(){
+	global $wp_rewrite;
+   	$wp_rewrite->flush_rules();
+}
+
+// Adding a new rule
+function wp_insertLMRewriteRules($rules)
+{
+	$LM_page = get_option('lm_url');
+	if ($LM_page===false || empty($LM_page)) $LM_page = 'linksmanager';
+	
+	$newrules = array();
+	$newrules['^'.$LM_page.'/(.*)$'] = 'index.php?pagename='.$LM_page.'&route=$matches[1]';
+	
+	return $newrules + $rules;
+}
+
+// Adding the 'route' var so that WP recognizes it
+function wp_insertLMRewriteQueryVars($vars)
+{
+    array_push($vars, 'route');
+    return $vars;
+}
+?>
